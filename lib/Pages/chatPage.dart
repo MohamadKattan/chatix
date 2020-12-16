8d22d3cd3809a087faf24f3f663a62163015f9b2
Mohamad <kattansoftware@gmail.com>
2020-12-16 14:42:19 -0800

    16/12/2020

--- lib/Pages/chatPage.dart
+++ lib/Pages/chatPage.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chatix/Widgets/progressWidget.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
 
 // this class for custom appBar for show photo + name user who will start chating
 class Chat extends StatelessWidget {
@@ -110,6 +116,17 @@ class ChatScreenState extends State<ChatScreen> {
       {Key key, @required this.receiverId, @required this.receiverAveter});
   final TextEditingController textEditingController = TextEditingController();
   final FocusNode focusNode = FocusNode();
+  bool isDisplayStiker;
+  bool isloading;
+  File imageFile;
+  String imageUrl;
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(onFoucusChange);
+    isDisplayStiker = false;
+    isloading = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -119,15 +136,18 @@ class ChatScreenState extends State<ChatScreen> {
           Column(
             children: [
               createListMessages(),
-              //this method for textField and buttons Image+ emogel
+              (isDisplayStiker ? createStecker() : Text('')),
               createInput(),
             ],
-          )
+          ),
+          createisloading(),
         ],
       ),
+      onWillPop: onBackPress,
     );
   }
 
+// this method include textified+ buttons
   createInput() {
     return Container(
       width: double.infinity,
@@ -143,7 +163,7 @@ class ChatScreenState extends State<ChatScreen> {
             child: Container(
               margin: EdgeInsets.symmetric(horizontal: 8.0),
               child: IconButton(
-                onPressed: () => print('1'),
+                onPressed: getStecker,
                 icon: Icon(
                   Icons.face,
                   color: Colors.teal[600],
@@ -167,7 +187,7 @@ class ChatScreenState extends State<ChatScreen> {
             child: Container(
               margin: EdgeInsets.symmetric(horizontal: 1.0),
               child: IconButton(
-                onPressed: () => print('1'),
+                onPressed: () => onSendMessage(textEditingController.text, 0),
                 icon: Icon(
                   Icons.send,
                   color: Colors.teal[600],
@@ -180,7 +200,7 @@ class ChatScreenState extends State<ChatScreen> {
             child: Container(
               margin: EdgeInsets.symmetric(horizontal: 1.0),
               child: IconButton(
-                onPressed: () => print('1'),
+                onPressed: getImage,
                 icon: Icon(
                   Icons.image,
                   color: Colors.teal[600],
@@ -193,6 +213,7 @@ class ChatScreenState extends State<ChatScreen> {
     );
   }
 
+// this method for showing chanting
   createListMessages() {
     return Flexible(
         child: Center(
@@ -200,4 +221,183 @@ class ChatScreenState extends State<ChatScreen> {
       valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
     )));
   }
+
+// this method for if we click on text field for twist from sticker showing to keypord
+  onFoucusChange() {
+    if (focusNode.hasFocus) {
+      setState(() {
+        isDisplayStiker = false;
+      });
+    }
+  }
+
+// this method for container include sticker
+  createStecker() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: Colors.grey, width: 0.5)),
+        color: Colors.white,
+      ),
+      padding: EdgeInsets.all(5.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                  onPressed: () => onSendMessage('bear1', 2),
+                  child: Image.asset(
+                    'images/bear1.jpg',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+              FlatButton(
+                  onPressed: () => onSendMessage('bear2', 2),
+                  child: Image.asset(
+                    'images/bear2.png',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+              FlatButton(
+                  onPressed: () => onSendMessage('bear3', 2),
+                  child: Image.asset(
+                    'images/bear3.png',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                  onPressed: () => onSendMessage('bear1', 2),
+                  child: Image.asset(
+                    'images/bear1.jpg',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+              FlatButton(
+                  onPressed: () => onSendMessage('bear2', 2),
+                  child: Image.asset(
+                    'images/bear2.png',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+              FlatButton(
+                  onPressed: () => onSendMessage('bear3', 2),
+                  child: Image.asset(
+                    'images/bear3.png',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                  onPressed: () => onSendMessage('bear1', 2),
+                  child: Image.asset(
+                    'images/bear1.jpg',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+              FlatButton(
+                  onPressed: () => onSendMessage('bear2', 2),
+                  child: Image.asset(
+                    'images/bear2.png',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+              FlatButton(
+                  onPressed: () => onSendMessage('bear3', 2),
+                  child: Image.asset(
+                    'images/bear3.png',
+                    height: 50.0,
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  )),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+//this method whem we will click on Sticker button it will show or close list Stickers
+  getStecker() {
+    focusNode.unfocus();
+    setState(() {
+      isDisplayStiker = !isDisplayStiker;
+    });
+  }
+
+  // after sending stecker it will close list sticker
+  Future<bool> onBackPress() {
+    if (isDisplayStiker) {
+      setState(() {
+        isDisplayStiker = false;
+      });
+    } else {
+      Navigator.pop(context);
+    }
+    return Future.value(false);
+  }
+
+// this method for circuler progsses
+  createisloading() {
+    return Positioned(
+      child: isloading ? circularProgres() : Text(''),
+    );
+  }
+
+  //this method for pick an image from gallery
+  Future getImage() async {
+    File newImageFile =
+        await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (newImageFile != null) {
+      setState(() {
+        this.imageFile = newImageFile;
+        isloading = true;
+      });
+    }
+    uploadImageFile();
+  }
+
+// this method for upload image sender to Storage+firebase
+  Future uploadImageFile() async {
+    try {
+      String fileName = DateTime.now().toString();
+      StorageReference storageReference =
+          FirebaseStorage.instance.ref().child('chatImage').child(fileName);
+      StorageUploadTask storageUploadTask = storageReference.putFile(imageFile);
+      StorageTaskSnapshot storageTaskSnapshot =
+          await storageUploadTask.onComplete;
+      storageTaskSnapshot.ref.getDownloadURL().then((value) => (downloadUrl) {
+            imageUrl = downloadUrl;
+            setState(() {
+              isloading = false;
+              onSendMessage(imageUrl, 1);
+            });
+          });
+    } catch (ex) {
+      setState(() {
+        isloading = false;
+      });
+      Fluttertoast.showToast(msg: 'Errore:' + ex.toString());
+    }
+  }
+
+// this method for save on firebase all (chat data :text+.....else)
+  void onSendMessage(String contentMsg , int type) {}
 }
